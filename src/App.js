d43ccc4e1ed2ecc9bd32170c053c49565b4de316
Lade06 <oladunjoyeololade@gmail.com>
2023-08-26 12:56:16 +0100

    working on the homepage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 
+// import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Login from './Components/login';
+import Login from './pages/login';
+
+
 
 
 function App() {
   return (
     <div>
-      
-<Login />
-
+     {/* <BrowserRouter> */}
+      {/* <Routes> */}
+        {/* <Route path="/" element={<Home />} /> */}
+          {/* <Route index element={<Home />} /> */}
+          {/* <Route path="login" element={<Login />} /> */}
+          {/* <Route path="signup" element={<Sig />} /> */}
+          {/* <Route path="*" element={<NoPage />} /> */}
+       
+      {/* </Routes> */}
+     {/* </BrowserRouter> */}
+    
+  <Login />
     </div>
   );
 }
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,21 +1,150 @@
 import React, { useState } from "react";
-import Navbar from "./navbar";
+// import Navbar from "../Components/navbar";
+import code from "../Assets/code.jpeg";
+import sewing from "../Assets/sewing.jpg";
+import hair from "../Assets/hair.jpeg";
+import laglogo from "../Assets/lagos-logo.png";
 
 const Login = () => {
-  const [login, setLogin] = useState(true);
+  const [login, setLogin] = useState(false);
   const [signUp, setSignUp] = useState(false);
+  const [homePage, setHomePage] = useState(true);
 
   const handleShowLogin = () => {
     setLogin(true);
     setSignUp(false);
+    setHomePage(false);
   };
   const handleShowSignUp = () => {
     setLogin(false);
     setSignUp(true);
+    setHomePage(false);
+  };
+  const handleShowHomePage = () => {
+    setLogin(false);
+    setHomePage(true);
   };
   return (
     <div>
-      <Navbar />
+      {/* <Navbar /> */}
+
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-mdb-toggle="collapse"
+            data-mdb-target="#navbarLeftAlignExample"
+            aria-controls="navbarLeftAlignExample"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <i className="fas fa-bars"></i>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarLeftAlignExample">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/">
+                  About
+                </a>
+              </li>
+
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle  btn-primary"
+                  href="/"
+                  id="navbarDropdown"
+                  role="button"
+                  data-mdb-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Department
+                </a>
+
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li>
+                    <a className="dropdown-item  " href="/">
+                      Computer
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="/">
+                      Catering
+                    </a>
+                  </li>
+                  <li>
+                    <hr className="dropdown-divider" />
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="/">
+                      Photography
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="/">
+                      Fashion
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="/">
+                      Textile
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link " href="/">
+                  Contact
+                </a>
+              </li>
+              <li className="nav-item login-signup">
+                <button className="btn btn-primary" onClick={handleShowLogin}>
+                  Login
+                </button>
+              </li>
+              <li className="nav-item login-signup btn-signup mx-3">
+                <button className="btn" onClick={handleShowSignUp}>
+                  Sign Up
+                </button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      {homePage && (
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6 mt-5">
+              <img src={laglogo} alt="" width={"100%"} />
+            </div>
+            <div className="col-md-6 mt-5">
+              <h1>
+                Welcome To <br /> WOMEN DEVELOMENT CENTRE AGEGE
+              </h1>
+              <h2>Ministry Of Women Affairs & Poverty Alleviation</h2>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-4 mt-5">
+              <img src={sewing} alt="" width={"100%"} />
+            </div>
+            <div className="col-md-4 mt-5">
+              <img src={code} alt="" width={"100%"} />
+            </div>
+            <div className="col-md-4 mt-5">
+              <img src={hair} alt="" width={"100%"} />
+            </div>
+          </div>
+        </div>
+      )}
+
       {login && (
         <div className="container">
           <div className="row">
@@ -79,7 +208,10 @@ const Login = () => {
                     <a href="/">Forgot password?</a>
                   </div>
                 </div>
-                <button className="btn btn-primary btn-block form-control">
+                <button
+                  className="btn btn-primary btn-block form-control"
+                  onClick={handleShowHomePage}
+                >
                   Login
                 </button>
                 <div className="text-center mt-4">
